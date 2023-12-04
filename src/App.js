cb26db175bc959d859d90b1036bc3fc02127fa6d
FIRST_NAME LAST_NAME <EMAIL_ADDRESS>
2023-12-04 11:10:30 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import MainController from './controller/MainController';
+
+function App() {
+  return (
+    <MainController></MainController>
+  );
+}
+
+export default App;
